3b15a529f33746148e78bdbae03b0ff8052d7b36
Bobby Kissinger <kissinger645@gmail.com>
2017-02-01 16:14:51 -0500

    Add files via upload

---
+++ WebFormsCalculator.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.css" rel="stylesheet" type="text/css" />
+    <title>Simple Calc</title>
+</head>
+<body>
+    <div style="width:800px; margin:0 auto;">
+        
+    
+    <form id="form1" runat="server">
+        <div>
+            First Number:<br />
+            <input type="number" name="First Number" value="" />
+            <br />
+        </div>
+
+        <div class="form-group input-lg">
+            <select name="Choice">
+                <% foreach (var option in Options)
+                    { %>
+                <option value="<%=option %>"><%= option %></option>
+                <% } %>
+            </select>
+        </div>
+        
+        <div>
+            Second Number:<br />
+            <input type="number" name="Second Number" value=""/>
+            <br />
+        </div>
+        <br />
+        <input type="submit" class="btn btn-default" value="Calculate"/>
+    </form>
+        <br />
+        <h1>The answer is <%=Answer%></h1>
+</div>
+</body>
+</html>
---
+++ WebFormsCalculator.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public float First;
+    public float Second;
+    public float Answer = 0;
+    public string Choice;
+    public List<string> Options = new List<string>() { "+", "-", "*", "/" };
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //If Page_Load is a result of a form submission...
+        if (IsPostBack)
+        {
+            First = float.Parse(Request.Form["First Number"]);
+            Second = float.Parse(Request.Form["Second Number"]);
+            Calculate(First, Second);
+        }
+    }
+    public void Calculate(float _first, float _second)
+    {
+        switch (Choice)
+        {
+            case "-":
+                Answer = First - Second;
+                break;
+            case "*":
+                Answer = First * Second;
+                break;
+            case "/":
+                Answer = First / Second;
+                break;
+            default:
+                Answer = First + Second;
+                break;
+        }
+    }
+
+}
